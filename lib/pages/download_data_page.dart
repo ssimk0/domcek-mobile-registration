10f3b0df308f632e3d59ea64836b92cbd9cb9fd0
Sebastian Simko <sebastian@adaptiware.com>
2019-05-18 06:47:39 +0200

    setup for release

--- lib/pages/download_data_page.dart
+++ lib/pages/download_data_page.dart
@@ -25,42 +25,45 @@ class DownloadDataPageState extends State<DownloadDataPage> {
           title: Text('Stiahnuť data'),
         ),
         body: Center(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                _error != '' && _error != null
-                    ? Text(_error, style: TextStyle(color: Colors.red))
-                    : Text(''),
-                TextFormField(
-                  controller: tokenController,
-                  decoration: InputDecoration(labelText: 'Zadaj svoj token'),
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Token musí byť vyplnení';
-                    }
-                  },
-                ),
-                RaisedButton(
-                    color: Colors.blue,
-                    textColor: Colors.white,
-                    splashColor: Colors.blueGrey,
-                    onPressed: () async {
-                      if (_formKey.currentState.validate()) {
-                        try {
-                          await model.fetchParticipants(tokenController.text);
-                          Navigator.pop(context);
-                        } catch (e) {
-                          print(e);
-                          _error =
-                              'Nepodarilo sa stiahnuť data, skus znova alebo kontaktuj niekoho s registracie';
-                        }
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  _error != '' && _error != null
+                      ? Text(_error, style: TextStyle(color: Colors.red))
+                      : Text(''),
+                  TextFormField(
+                    controller: tokenController,
+                    decoration: InputDecoration(labelText: 'Zadaj svoj token'),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Token musí byť vyplnení';
                       }
                     },
-                    child: const Text('Stiahnuť Data')),
-              ],
+                  ),
+                  RaisedButton(
+                      color: Colors.blue,
+                      textColor: Colors.white,
+                      splashColor: Colors.blueGrey,
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          try {
+                            await model.fetchParticipants(tokenController.text);
+                            Navigator.pop(context);
+                          } catch (e) {
+                            print(e);
+                            _error =
+                                'Nepodarilo sa stiahnuť data, skus znova alebo kontaktuj niekoho s registracie';
+                          }
+                        }
+                      },
+                      child: const Text('Stiahnuť Data')),
+                ],
+              ),
             ),
           ),
         ),
